e2ddd3207840ef1d7020b9391a79c6339eddc0b5
khabibulin.do1648 <khabibulin.do1648@MAST-407-TEACH>
2024-10-26 14:17:35 +0300

    Добавьте файлы проекта.

---
+++ MyCalcLib.Test/MyCalcTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyCalcLib.Test
+{
+    [TestClass]
+    public class MyCalcTest
+    {
+        [TestMethod]
+        public void Sum_10and20_30returned()
+        {
+            //arrange
+
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+
+            //act
+            MyCalc c = new MyCalc();
+
+            int actual = c.Sum(x, y);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Min_10and20_30returned()
+        {
+            int x = 50;
+            int y = 20;
+            int expected2 = 30;
+
+            MyCalc c = new MyCalc();
+
+            int actual2 = c.Min(x, y);
+
+            Assert.AreEqual(expected2, actual2);
+        }
+    }
+}
---
+++ MyCalcLib.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyCalcLib.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyCalcLib.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("912f7100-8438-45cd-9a81-1f663a036a35")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
